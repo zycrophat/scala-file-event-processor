8327db2e927b744fc32ba9cd52f2108e9f6cba84
Andreas Steffan <andreas.steffan@mailbox.org>
2020-07-15 21:49:12 +0200

    using plain old try-catch

--- scala-file-event-processor-function/src/main/scala/zycrophat/fileeventprocessor/MyFunction.scala
+++ scala-file-event-processor-function/src/main/scala/zycrophat/fileeventprocessor/MyFunction.scala
@@ -41,9 +41,11 @@ class MyFunction extends LazyLogging {
 
     val container = CosmosDb.dbClient.getDatabase("blobfunctionsdb").getContainer("blobfunctionsContainer1")
 
-    Try(container.upsertItem(writeToString(FileMetadata(name = fileName, timestamp = LocalDateTime.now())))) match {
-      case Success(r) => logger.info(s"CosmosDB update succeeded. Status code: ${r.getStatusCode}")
-      case Failure(t) => logger.error(s"CosmosDB upsert failed", t)
+    try {
+      val r = container.upsertItem(writeToString(FileMetadata(name = fileName, timestamp = LocalDateTime.now())))
+      logger.info(s"CosmosDB update succeeded. Status code: ${r.getStatusCode}")
+    } catch {
+      case t: Throwable => logger.error(s"CosmosDB upsert failed", t)
     }
 
   }
