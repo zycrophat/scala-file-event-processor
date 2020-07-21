a91cb15760a7560d815297e541138ae7647899fe
Andreas Steffan <andreas.steffan@mailbox.org>
2020-07-22 00:48:13 +0200

    added AI-Agent.xml to improve log event correlation

--- scala-file-event-processor-function/src/main/scala/zycrophat/fileeventprocessor/MyFunction.scala
+++ scala-file-event-processor-function/src/main/scala/zycrophat/fileeventprocessor/MyFunction.scala
@@ -3,14 +3,13 @@ package zycrophat.fileeventprocessor
 import java.security.MessageDigest
 import java.time.LocalDateTime
 
-import com.azure.cosmos.models.CosmosItemResponse
-import com.azure.cosmos.{CosmosAsyncClient, CosmosClient, CosmosClientBuilder}
-import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import com.azure.cosmos.{CosmosClient, CosmosClientBuilder}
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, _}
+import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.microsoft.azure.functions.ExecutionContext
 import com.microsoft.azure.functions.annotation.{BindingName, BlobTrigger, FunctionName}
 import com.typesafe.scalalogging.LazyLogging
-import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, _}
-import com.github.plokhotnyuk.jsoniter_scala.macros._
 
 import scala.util.{Failure, Success, Try}
 
@@ -35,7 +34,6 @@ class MyFunction extends LazyLogging {
   def run(@BlobTrigger(connection = "storageConn", dataType = "", name = "myblob", path = "blobs/{name}") myblob: String,
           @BindingName("name") fileName: String,
            context: ExecutionContext): Unit = {
-
     context.getLogger.info("Scala trigger processed a request.")
     logger.info(s"CosmosDb: ${CosmosDb.dbClient.isDefined}")
     logger.info(s"Foobar1339: $fileName")
